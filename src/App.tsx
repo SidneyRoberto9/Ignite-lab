abc2b40e2cb61250699d14b27660be551d8cf98f
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-21 20:45:54 -0300

    ✨ feat: Dia 2
    Nessa aula nós vamos desenvolver a estrutura visual do projeto e nos aprofundar mais ainda nos conceitos e oportunidades que você vai ter por virar um especialista em ReactJS. Recomendo que você foque muito para chegar até o final da missão e desenvolver a aplicação completa.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        name
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import Event from './pages/Event';
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
-
-  return (
-    <ul className="flex gap-4">
-      {data?.lessons.map((lesson) => {
-        return (
-          <li className="mt-4 ml-4" key={lesson.id}>
-            {lesson.title}
-          </li>
-        );
-      })}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { client } from './lib/apollo';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
